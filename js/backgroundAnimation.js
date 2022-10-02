641fc93720b5959487b67bbb0fc1ed6b38970565
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2022-10-02 16:56:04 +0100

    updated website

--- js/backgroundAnimation.js
+++ js/backgroundAnimation.js
@@ -1,20 +1,10 @@
 
 // constants
 const COLOR_SPACE = "#040403";
-// const COLOR_STARS = "rgba(153, 136, 136, 0.5)";
-// const COLOR_STARS = "rgba(100, 100, 100, 0.8)";
 const COLOR_STARS = "rgba(255, 255, 255, 0.9)";
-
-// const COLOR_SPACE = "#EADEDA";
-// const COLOR_STARS = "rgba(153, 136, 136, 0.5)";
-
-// const STAR_NUM = 100; // number of stars in the starfield
-// const STAR_SIZE = 0.02; // max star size as a fraction of screen width
-// const STAR_SPEED = 0.01; // fraction of screen width per second
 const STAR_NUM = 300; // number of stars in the starfield
 const STAR_SIZE = 0.002; // max star size as a fraction of screen width
 const STAR_SPEED = 0.012; // fraction of screen width per second
-// const STAR_SIZE = 0.050;
 
 // set up the canvas and context
 var canvas = document.createElement("canvas");
@@ -61,7 +51,6 @@ function loop(timeNow) {
 
     // draw the stars
     ctx.fillStyle = COLOR_STARS;
-    // ctx.fillStyle = generateRandomOpacity();
     for (let i = 0; i < STAR_NUM; i++) {
         ctx.beginPath();
         ctx.arc(stars[i].x, stars[i].y, stars[i].r, 0, Math.PI * 2);
@@ -94,34 +83,48 @@ function loop(timeNow) {
     //     starYCoords = []
 
     //     // console.log("System is paused");
-    //     stars.splice(0,stars.length);
+    //     // stars.splice(0,stars.length);
 
     //     generateStars();
     //     // console.log("Stars regen");
     // }
 
+    // window.onfocus = function () {
+    //     stars.splice(0,stars.length);
+    //     generateStars();
+    // };
+
     // call the next frame
     requestAnimationFrame(loop);
-}
-
-function loopDuringPause() {
-    console.log("paused");
 }
 
 function randomSign() {
     return Math.random() >= 0.5 ? 1 : -1;
 }
 
-function generateRandomColor() {
-    var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-    if(randomColor.length != 7){ // In any case, the color code is invalid
-        randomColor = generateRandomColor();
-    }
-    return randomColor;
-    // The random color will be freshly served
+function resizeBGCanvas() {
+    var remover = document.getElementsByClassName('background-animation')[0];
+    remover.remove();
+
+    var canvas = document.createElement("canvas");
+    var ctx = canvas.getContext("2d");
+    canvas.height = document.documentElement.clientHeight;
+    canvas.width = document.documentElement.clientWidth;
+    document.getElementById("background-animation").appendChild(canvas);
+
+    // set up the stars
+    // var stars = [];
+    // var starSpeed = STAR_SPEED * canvas.width;
+    // var xv = starSpeed * randomSign() * Math.random();
+    // // Using Pythagoras' theorem, yv = sqrt(starSpeed^2 - xv^2)
+    // var yv = Math.sqrt(Math.pow(starSpeed, 2) - Math.pow(xv, 2)) * randomSign();
+    generateStars();
+    requestAnimationFrame(loop);
 }
 
-function generateRandomOpacity() {
-    var opacityValue = Math.floor(Math.random() * (1 - 0.5 + 1)) + 0.5;
-    return "rgba(0, 0, 0, "+ opacityValue +")";
-}
+// window.onresize = (event) => {
+//     console.log(event)
+//     resizeBGCanvas();
+// };
+
+
