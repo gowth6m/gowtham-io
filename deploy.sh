cd7d12a8ba38d0b131b8c066a55714c6f203964a
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2022-12-12 17:23:36 +0000

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set the file name
+FILE_NAME="CNAME"
+
+# Check if the file exists
+if [ ! -f "$FILE_NAME" ]; then
+    echo "Error: File does not exist"
+    exit 1
+fi
+
+# Check if Git is installed
+if ! [ -x "$(command -v git)" ]; then
+    echo "Error: Git is not installed"
+    exit 1
+fi
+
+# Check if the current directory is a Git repository
+if ! [ -d ".git" ]; then
+    echo "Error: Not a Git repository"
+    exit 1
+fi
+
+# Add the file to the Git repository
+git add "$FILE_NAME"
+
+# Commit the changes
+git commit -m "Added $FILE_NAME to the gh-pages branch"
+
+# Push the changes to the apple branch on GitHub
+git push origin gh-pages
